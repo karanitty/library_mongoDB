bce096e0a7f6d0be2c1304458d6da0fa752c11db
karanitty <karanitty@gmail.com>
2021-05-11 18:45:47 +0530

    ok

--- src/model/authorData.js
+++ src/model/authorData.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 //Database connection
 mongoose.connect('mongodb+srv://userone:userone@fsd.vpxw6.mongodb.net/library?retryWrites=true&w=majority');
+// mongoose.connect('mongodb://localhost:27017/library');
 
 //Schema definition
 const schema = mongoose.Schema;
